e0fd51c85491e193dfcdbdf541f7d90e1292a8f1
IngCampos <15151130@mail.ita.mx>
2019-02-19 01:52:14 -1200

    agregacion Add_point()
    Agrega los 2 valores junto con un color para representarlos en la grafica.
    la matriz es de prueba
    Estructura basica de entradas

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-﻿$(document).ready(function(){
-        $(".ver_top").hide();
-    $(".limpiar_codigo").click(function(){
-alert("limpiar-codigo");
-});
-$(".ver_top").click(function(){
-
-});
+﻿var points = [['10','20','#ff0000'],['10','20','#00ff00'],['10','20','#0000ff'],['10','20','#ffff00']];
+function Add_point(){
+    points.push([$("#value-x").val() ,$("#value-y").val() , $("#value-color").val()])
+console.log(points);
+$("#texto").text(points);    
+}
+$(document).ready(function(){
+    $("#value-x").val("0");
+    $("#value-y").val("0");
 });
