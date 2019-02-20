e62998fe48c35165240ad494baae31efaaa43752
IngCampos <15151130@mail.ita.mx>
2019-02-20 05:22:23 -1200

    Limpieza de codigo
    Acomodo de framework.
    Separacion de codigo javascript de HTML
    Otros

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var random = [];
 function Add_point(){
     points.push([parseInt($("#value-x").val()) ,parseInt($("#value-y").val())]);
     Grafica(points, random);
-$("#texto").text(points);    
+console.log(points);    
 }
 function Add_random(){
   random = [];
@@ -15,4 +15,73 @@ $(document).ready(function(){
     Grafica();
     $("#value-x").val("0");
     $("#value-y").val("0");
-});
+});function Grafica(points, random){
+  Highcharts.chart('container', {
+      chart: {
+        type: 'scatter',
+        zoomType: 'xy'
+      },
+      title: {
+        text: 'Table points'
+      },
+      xAxis: {
+        title: {
+          enabled: true,
+          text: 'Grades'
+        },
+        startOnTick: true,
+        endOnTick: true,
+        showLastLabel: true
+      },
+      yAxis: {
+        title: {
+          text: 'Time'
+        }
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        x: 100,
+        y: 60,
+        floating: true,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+        borderWidth: 1
+      },
+      plotOptions: {
+        scatter: {
+          marker: {
+            radius: 5,
+            states: {
+              hover: {
+                enabled: true,
+                lineColor: 'rgb(100,100,100)'
+              }
+            }
+          },
+          states: {
+            hover: {
+              marker: {
+                enabled: false
+              }
+            }
+          },
+          tooltip: {
+            headerFormat: '<b>{series.name}</b><br>',
+            pointFormat: '{point.x} grade, {point.y} minutes'
+          }
+        }
+      },
+      series: [{
+        name: 'Points',
+        color: 'rgba(0, 0, 255, .8)',
+        data: points
+      },
+      {
+        name: 'Clusters',
+        color: 'rgba(0, 255, 0, .8)',
+        data: random
+      },]
+    });
+  }
+  
