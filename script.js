e98265c91a9fc5fcc5625677a89003d69f36254c
IngCampos <15151130@mail.ita.mx>
2019-02-20 09:52:36 -1200

    Mejora codigo
    Colores para la tabla
    valores prueba del cluster
    Funcion Grafica_color() terminada
    funcion Grafica2() en proceso

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 ﻿var points = [];
 var random = [];
-var pointsColor= []
+var cluster_values = [[[10,23],[12,60]],[[19,2],[12,43]],[[12,53],[8,5]]];//ejemplo
+var clusterColors = ['rgba(255, 0, 0, .8)','rgba(0, 255, 0, .8)','rgba(0, 0, 255, .8)','rgba(255, 255, 0, .8)','rgba(255, 0, 255, .8)',
+'rgba(0, 255, 255, .8)','rgba(0, 125, 200, .8)','rgba(255, 125, 125, .8)','rgba(125, 125, 255, .8)','rgba(255, 125, 255, .8)'];
+clusterColors.sort(function() {return Math.random() - 0.5});
 
 function Add_point(){
     points.push([parseInt($("#value-x").val()) ,parseInt($("#value-y").val())]);
     Grafica(points, random);
 console.log(points);    
 }
+
 function Add_random(){
   random = [];
   random.push([Math.round(Math.random()*100) ,Math.round(Math.random()*60)]);
   random.push([Math.round(Math.random()*100) ,Math.round(Math.random()*60)]);
 Grafica(points, random);
 }
+
 $(document).ready(function(){
     Grafica();
     $("#value-x").val("0");
     $("#value-y").val("0");
 });
+
 function Grafica(points, random){
   Highcharts.chart('grafica', {
       chart: {
@@ -77,25 +83,31 @@ function Grafica(points, random){
       },
       series: [{
         name: 'Points',
-        color: 'rgba(0, 0, 255, .8)',
+        color: 'rgba(125, 125, 125, .8)',
         data: points
       },
       {
         name: 'Clusters',
-        color: 'rgba(0, 255, 0, .8)',
+        color: 'rgba(0, 0, 0, 1)',
         data: random
       },]
     });
   }
+
   function Grafica_color(clusters){
-    pointsColor= []
+    var pointsColor= []
+    pointsColor.push({
+      name: 'Clusters-points',
+      color: 'rgba(255, 255, 255, .8)',
+      data: random
+    });
     for(var a=0; a<clusters ;a++){
     pointsColor.push({
       name: "Cluster " +(a+1),
-      color: 'rgba(0, 0, 255, .8)',
-      data: points
-    })
+      color: clusterColors[a],
+      data: cluster_values[a],
+    });
   }
-  console.log(pointsColor);
+  return pointsColor;
+  }
 
-  }
