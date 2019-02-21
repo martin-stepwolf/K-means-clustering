964d4383df1792441e94eee5379da66d5e2976e9
IngCampos <15151130@mail.ita.mx>
2019-02-20 14:30:02 -1200

    Valores random

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 ﻿var points = [];
+var random = [];
 function Add_point(){
     points.push([parseInt($("#value-x").val()) ,parseInt($("#value-y").val())]);
-    Grafica(points);
-console.log(points);
+    Grafica(points, random);
 $("#texto").text(points);    
+}
+function Add_random(){
+  random = [];
+  random.push([Math.round(Math.random()*100) ,Math.round(Math.random()*60)]);
+  random.push([Math.round(Math.random()*100) ,Math.round(Math.random()*60)]);
+Grafica(points, random);
 }
 $(document).ready(function(){
     Grafica();
