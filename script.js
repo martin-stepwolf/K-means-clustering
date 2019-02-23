1640d1c8130b3553772e24375c8b8b79c4928bee
IngCampos <15151130@mail.ita.mx>
2019-02-22 17:08:58 -1200

    Mejora programa
    Metodo chuy
    Valores iniciales
    Simplificacion de codigo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿var points = [];
+﻿var points = [[0,0],[60,100],[15,25],[45,75],[30,50]];
 var random = [];
 var random_ant=[];
 var cluster_values = [];
@@ -8,7 +8,7 @@ clusterColors.sort(function() {return Math.random() - 0.5});
 
 function Add_point(){
     points.push([parseInt($("#value-x").val()) ,parseInt($("#value-y").val())]);
-    Grafica(points, random);
+    Grafica();
     $("#texto").text(points);
     console.log(points);    
 }
@@ -18,11 +18,10 @@ function Add_random(){
     var that = 0;
     random = [rando];
 	for(var i = 0; i<rando; i++){
-	random.push([Math.round(Math.random()*100) ,Math.round(Math.random()*60)]);
+	random.push([Math.round(Math.random()*60) ,Math.round(Math.random()*100)]);
 	}
 
 //Grafica(points, random);
-
 med_asig();
 Grafica2();
 }
@@ -140,7 +139,7 @@ $(document).ready(function(){
    $.get('Quiz 1 grades.csv', function(csvFile){
         points= parseCSVData(csvFile);
         var rand = [];
-        Grafica(points);
+        Grafica();
     });
 
     function parseCSVData(csvFile){
@@ -166,7 +165,7 @@ $(document).ready(function(){
     }
 });
 
-function Grafica(points, random){
+function Grafica(){
   Highcharts.chart('grafica', {
       chart: {
         type: 'scatter',
@@ -340,5 +339,19 @@ if((parseInt($("#value-x").val())>=101) || (parseInt($("#value-y").val())>=61)){
   alert("Invalid values. You need to add a grade less than 100 and time less than 60.");
 }else if(Add_point());
 
+}
+
+function Promedio(){
+  for(var j=0; j<random.length; j++){
+  var x=0;
+  var y=0;
+  for(var i=0; i<cluster_values[j].length; i++){
+    x=x+cluster_values[j][i][0];
+    y=y+cluster_values[j][i][1];
+  }
+  var xx=x/cluster_values[j].length;
+  var yy=y/cluster_values[j].length;
+  random_ant[j] = [xx,yy]; 
+  }
 }
 	
